27da4d18a653125dc5ddc182bd0d63719beed647
Toby Tang <tobykuxn@icloud.com>
2022-07-15 07:23:37 +0800

    69. Binding to Custom Events

--- cmp-databinding-start/src/app/app.component.ts
+++ cmp-databinding-start/src/app/app.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   serverElements = [{type: "server", name: "Testserver", content: "Just a test!"}];
+
+  onServerAdded(serverDate: {
+    serverName: string,
+    serverContent: string
+  }) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverDate.serverName,
+      content: serverDate.serverContent
+    });
+  }
+
+  onBlueprintAdded(blueprintDate: {
+    serverName: string,
+    serverContent: string
+  }) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintDate.serverName,
+      content: blueprintDate.serverContent
+    });
+  }
 }
